963921ecfd604312a1588872646a1fdcd1b7c350
asberg95 <a.s.van.den.berg@fsw.leidenuniv.nl>
2026-06-08 10:19:16 +0000

    Deploying to gh-pages from @ asberg95/Social_Inequality_Lab@d9741bb1a5cb35f817e5c06634394d5aba4a08a6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -80,9 +80,9 @@ ninja.data = [{
           description: "....",
           section: "Projects",handler: () => {
               window.location.href = "/Social_Inequality_Lab/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
+            },},{id: "projects-income-volatility-psychological-states-and-saving-decisions",
+          title: 'Income Volatility, Psychological States, and Saving Decisions',
+          description: "....",
           section: "Projects",handler: () => {
               window.location.href = "/Social_Inequality_Lab/projects/6_project/";
             },},{id: "projects-project-7",
